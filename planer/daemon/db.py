d83a33b69c56e4b9f912f62af3fbb38718638fcf
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-11-11 23:16:41 +0100

    add initial database schema

---
+++ planer/daemon/db.py
@@ -0,0 +1,29 @@
+
+from datetime import datetime
+from pony.orm import Database, PrimaryKey, Required, Set, Optional, sql_debug
+
+db = Database("sqlite", "database.sqlite", create_db=True)
+
+class Calendar(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    events = Set("Event")
+
+
+class GoogleCalendar(db.Calendar):
+    synch_token = Required(str, nullable=True)
+
+
+class Event(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    summary = Required(str)
+    description = Optional(str, default="")
+    start_time = Required(datetime)
+    end_time = Required(datetime)
+    location = Optional(str)
+    calendar = Required(Calendar)
+
+
+sql_debug(True)
+db.generate_mapping(create_tables=True)
+
